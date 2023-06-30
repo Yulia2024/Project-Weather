caf03529fc0aba25219e878230676b99bf0742ac
Yulia <Popov21042012@gmail.com>
2023-06-30 23:45:36 +0300

    Update Js

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let forecast = response.data.daily;
+
   forecast.forEach(function (forecastDay) {
-    forecastHTML += `
+    forecastHTML =
+      forecastHTML +
+      `
   
           <div class="col-2">
-            <div class="weather-forecast-date">${forecastDay.dt}</div>
+            <div class="weather-forecast-date">${formatDay(
+              forecastDay.dt
+            )}</div>
             <img
-              src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+              src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png"
               alt=""
               width="42"
             />
             <div class="weather-forecast-temperature">
-              <span class="weather-forecast-temperature-max"> ${forecastDay.temp.max}° </span>
-              <span class="weather-forecast-temperature-min"> ${forecastDay.temp.min}° </span>
+              <span class="weather-forecast-temperature-max"> ${
+                forecastDay.temp.max
+              }° 
+              </span>
+              <span class="weather-forecast-temperature-min"> ${
+                forecastDay.temp.min
+              }° 
+              </span>
             </div>
           </div>
           
@@ -26,10 +46,9 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "572d4e13399858af752a88640c3a56dd";
-  let apiLink = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiLink).then(displayForecast);
+  let apiKey = "cd173a006b0e51dac58c6d8064c94178";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 function displayWheatherCondition(response) {
   let currentCity = document.querySelector("#currentCity");
@@ -57,7 +76,7 @@ function displayWheatherCondition(response) {
 
 function search(event) {
   event.preventDefault();
-  let apiKey = "572d4e13399858af752a88640c3a56dd";
+  let apiKey = "cd173a006b0e51dac58c6d8064c94178";
   let city = document.querySelector("#text").value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(displayWheatherCondition);
@@ -93,3 +112,4 @@ if (day === 6) {
 }
 
 today.innerHTML = `${day} ${hours}:${minutes}`;
+search("New York");
